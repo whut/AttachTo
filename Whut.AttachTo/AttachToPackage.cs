9c61b3fec50eefb4d47bac2ba34b56f1c2bd2b64
whut <rafalklys@wp.pl>
2012-02-08 21:16:50 +0100

    Removed duplication from registering commands
    This also drops feature: attach to nunit-agent.exe, and if not found to nunit.exe

--- Whut.AttachTo/AttachToPackage.cs
+++ Whut.AttachTo/AttachToPackage.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.Design;
+using System.Linq;
 using System.Runtime.InteropServices;
 using EnvDTE;
 using Microsoft.VisualStudio;
@@ -26,84 +26,28 @@ namespace Whut.AttachTo
 
             OleMenuCommandService mcs = GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
 
-            this.AddAttachToIISCommand(mcs);
-            this.AddAttachToIISExpressCommand(mcs);
-            this.AddAttachToNUnitCommand(mcs);
+            this.AddAttachToCommand(mcs, PkgCmdIDList.cmdidWhutAttachToIIS, gop => gop.ShowAttachToIIS, "w3wp.exe");
+            this.AddAttachToCommand(mcs, PkgCmdIDList.cmdidWhutAttachToIISExpress, gop => gop.ShowAttachToIISExpress, "iisexpress.exe");
+            this.AddAttachToCommand(mcs, PkgCmdIDList.cmdidWhutAttachToNUnit, gop => gop.ShowAttachToNUnit, "nunit-agent.exe", "nunit.exe");
         }
 
-        private void AddAttachToIISCommand(OleMenuCommandService mcs)
+        private void AddAttachToCommand(OleMenuCommandService mcs, uint commandId, Func<GeneralOptionsPage, bool> isVisible, params string[] programsToAttach)
         {
-            CommandID attachToIISMenuCommandID = new CommandID(GuidList.guidAttachToCmdSet, (int)PkgCmdIDList.cmdidWhutAttachToIIS);
-            OleMenuCommand attachToIISMenuItem = new OleMenuCommand(
+            OleMenuCommand menuItemCommand = new OleMenuCommand(
                 delegate(object sender, EventArgs e)
                 {
                     DTE dte = (DTE)this.GetService(typeof(DTE));
                     foreach (Process process in dte.Debugger.LocalProcesses)
                     {
-                        if (process.Name.EndsWith("w3wp.exe"))
+                        if (programsToAttach.Any(p => process.Name.EndsWith(p)))
                         {
                             process.Attach();
                         }
                     }
                 },
-                attachToIISMenuCommandID);
-            attachToIISMenuItem.BeforeQueryStatus += (s, e) => attachToIISMenuItem.Visible = ((GeneralOptionsPage)this.GetDialogPage(typeof(GeneralOptionsPage))).ShowAttachToIIS;
-            mcs.AddCommand(attachToIISMenuItem);
-        }
-
-        private void AddAttachToIISExpressCommand(OleMenuCommandService mcs)
-        {
-            CommandID attachToIISExpressMenuCommandID = new CommandID(GuidList.guidAttachToCmdSet, (int)PkgCmdIDList.cmdidWhutAttachToIISExpress);
-            OleMenuCommand attachToIISExpressMenuItem = new OleMenuCommand(
-                delegate(object sender, EventArgs e)
-                {
-                    DTE dte = (DTE)this.GetService(typeof(DTE));
-                    foreach (Process process in dte.Debugger.LocalProcesses)
-                    {
-                        if (process.Name.EndsWith("iisexpress.exe"))
-                        {
-                            process.Attach();
-                        }
-                    }
-                },
-                attachToIISExpressMenuCommandID);
-            attachToIISExpressMenuItem.BeforeQueryStatus += (s, e) => attachToIISExpressMenuItem.Visible = ((GeneralOptionsPage)this.GetDialogPage(typeof(GeneralOptionsPage))).ShowAttachToIISExpress;
-            mcs.AddCommand(attachToIISExpressMenuItem);
-        }
-
-        private void AddAttachToNUnitCommand(OleMenuCommandService mcs)
-        {
-            CommandID attachToNUnitMenuCommandID = new CommandID(GuidList.guidAttachToCmdSet, (int)PkgCmdIDList.cmdidWhutAttachToNUnit);
-            OleMenuCommand attachToNUnitMenuItem = new OleMenuCommand(
-                delegate(object sender, EventArgs e)
-                {
-                    // NUnit runs test in nunit.exe or in separate process, nunit-agent.exe - it depends on configuration, target framework version, etc.
-                    // Code below attaches to nunit-agent.exe processes, but when it can't find any, it attaches to nunit.exe as fallback
-                    DTE dte = (DTE)this.GetService(typeof(DTE));
-                    List<Process> nunitProcesses = new List<Process>();
-                    bool nunitAgentFound = false;
-                    foreach (Process process in dte.Debugger.LocalProcesses)
-                    {
-                        if (process.Name.EndsWith("nunit-agent.exe"))
-                        {
-                            process.Attach();
-                            nunitAgentFound = true;
-                        }
-
-                        if (process.Name.EndsWith("nunit.exe"))
-                        {
-                            nunitProcesses.Add(process);
-                        }
-                    }
-
-                    if (!nunitAgentFound)
-                    {
-                        nunitProcesses.ForEach(p => p.Attach());
-                    }
-                },
-                attachToNUnitMenuCommandID);
-            attachToNUnitMenuItem.BeforeQueryStatus += (s, e) => attachToNUnitMenuItem.Visible = ((GeneralOptionsPage)this.GetDialogPage(typeof(GeneralOptionsPage))).ShowAttachToNUnit;
-            mcs.AddCommand(attachToNUnitMenuItem);
+                new CommandID(GuidList.guidAttachToCmdSet, (int)commandId));
+            menuItemCommand.BeforeQueryStatus += (s, e) => menuItemCommand.Visible = isVisible((GeneralOptionsPage)this.GetDialogPage(typeof(GeneralOptionsPage)));
+            mcs.AddCommand(menuItemCommand);
         }
     }
 }
