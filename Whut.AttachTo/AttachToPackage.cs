18d7c83b4764ed9b596448444bec93ceeb7e8d34
William Forney <wforney@hotmail.com>
2013-11-15 04:57:50 -0800

    Updated icons with default VS project icons from VS2013. Tweaked some things, added PostSharp for tracing.

--- Whut.AttachTo/AttachToPackage.cs
+++ Whut.AttachTo/AttachToPackage.cs
@@ -1,8 +1,27 @@
-﻿namespace Whut.AttachTo
+﻿using PostSharp.Patterns.Diagnostics;
+using PostSharp.Extensibility;
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AttachToPackage.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   This is the class that implements the package exposed by this assembly.
+//   The minimum requirement for a class to be considered a valid package for Visual Studio
+//   is to implement the IVsPackage interface and register itself with the shell.
+//   This package uses the helper classes defined inside the Managed Package Framework (MPF)
+//   to do it: it derives from the Package class that provides the implementation of the
+//   IVsPackage interface and uses the registration attributes defined in the framework to
+//   register itself and its components with the shell.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Whut.AttachTo
 {
     using System;
     using System.ComponentModel.Design;
+    using System.Diagnostics;
     using System.Diagnostics.Contracts;
+    using System.Globalization;
     using System.Linq;
     using System.Runtime.InteropServices;
 
@@ -11,12 +30,22 @@
     using Microsoft.VisualStudio;
     using Microsoft.VisualStudio.Shell;
 
+    using Process = EnvDTE.Process;
+
     /// <summary>
-    ///     Class AttachToPackage. This class cannot be inherited.
+    ///     This is the class that implements the package exposed by this assembly.
+    ///     The minimum requirement for a class to be considered a valid package for Visual Studio
+    ///     is to implement the IVsPackage interface and register itself with the shell.
+    ///     This package uses the helper classes defined inside the Managed Package Framework (MPF)
+    ///     to do it: it derives from the Package class that provides the implementation of the
+    ///     IVsPackage interface and uses the registration attributes defined in the framework to
+    ///     register itself and its components with the shell.
     /// </summary>
-    //// This attribute tells the PkgDef creation utility (CreatePkgDef.exe) that this class is a package.
+    //// This attribute tells the PkgDef creation utility (CreatePkgDef.exe) that this class is
+    //// a package.
     [PackageRegistration(UseManagedResourcesOnly = true)]
-    //// This attribute is used to register the information needed to show the this package in the Help/About dialog of Visual Studio.
+    //// This attribute is used to register the information needed to show this package
+    //// in the Help/About dialog of Visual Studio.
     [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
     //// This attribute is needed to let the shell know that this package exposes some menus.
     [ProvideMenuResource("Menus.ctmenu", 1)]
@@ -29,20 +58,34 @@
         #region Methods
 
         /// <summary>
-        ///     Initializes this instance.
+        ///     Initialization of the package; this method is called right after the package is sited, so this is the place
+        ///     where you can put all the initialization code that rely on services provided by VisualStudio.
         /// </summary>
         protected override void Initialize()
         {
+            Debug.WriteLine(string.Format(CultureInfo.CurrentCulture, "Entering Initialize() of: {0}", this));
             base.Initialize();
 
+            // Add our command handlers for menu (commands must exist in the .vsct file)
             var oleMenuCommandService = this.GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+            if (oleMenuCommandService == null)
+            {
+                return;
+            }
+            
+            // Create the command for the menu item.
+            this.AddAttachToCommand(
+                oleMenuCommandService, 
+                PkgCmdIDList.cmdidWhutAttachToIIS, 
+                gop => gop.ShowAttachToIIS, 
+                "w3wp.exe");
 
-            this.AddAttachToCommand(oleMenuCommandService, PkgCmdIDList.cmdidWhutAttachToIIS, gop => gop.ShowAttachToIIS, "w3wp.exe");
             this.AddAttachToCommand(
                 oleMenuCommandService, 
                 PkgCmdIDList.cmdidWhutAttachToIISExpress, 
                 gop => gop.ShowAttachToIISExpress, 
                 "iisexpress.exe");
+
             this.AddAttachToCommand(
                 oleMenuCommandService, 
                 PkgCmdIDList.cmdidWhutAttachToNUnit, 
@@ -71,7 +114,7 @@
         /// The programs automatic attach.
         /// </param>
         private void AddAttachToCommand(
-            OleMenuCommandService oleMenuCommandService, 
+            IMenuCommandService oleMenuCommandService, 
             uint commandId, 
             Func<GeneralOptionsPage, bool> isVisible, 
             params string[] programsToAttach)
@@ -81,19 +124,21 @@
             var menuItemCommand = new OleMenuCommand(
                 delegate
                     {
+                        // ReSharper disable once IdentifierTypo
                         var dte = (DTE)this.GetService(typeof(DTE));
-                        foreach (
-                            var process in
-                                dte.Debugger.LocalProcesses.Cast<Process>()
-                                    .Where(process => programsToAttach.Any(p => process.Name.EndsWith(p))))
+                        foreach (var process in
+                            dte.Debugger.LocalProcesses.Cast<Process>()
+                                .Where(process => programsToAttach.Any(p => process.Name.EndsWith(p))))
                         {
                             process.Attach();
                         }
                     }, 
                 new CommandID(GuidList.guidAttachToCmdSet, (int)commandId));
+            
             menuItemCommand.BeforeQueryStatus +=
                 (s, e) =>
                 menuItemCommand.Visible = isVisible((GeneralOptionsPage)this.GetDialogPage(typeof(GeneralOptionsPage)));
+
             oleMenuCommandService.AddCommand(menuItemCommand);
         }
 
--- Whut.AttachTo/Properties/AssemblyInfo.cs
+++ Whut.AttachTo/Properties/AssemblyInfo.cs
@@ -3,6 +3,10 @@ using System.Reflection;
 using System.Resources;
 using System.Runtime.InteropServices;
 
+using PostSharp.Extensibility;
+
+using Whut.AttachTo.Attributes;
+
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
@@ -19,13 +23,14 @@ using System.Runtime.InteropServices;
 [assembly: NeutralResourcesLanguage("en-US")]
 
 // Version information for an assembly consists of the following four values:
-//
 //      Major Version
 //      Minor Version 
 //      Build Number
 //      Revision
-//
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.1.0")]
 [assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly:
+    Trace(AttributeTargetTypes = "Whut.AttachTo", AttributeTargetTypeAttributes = MulticastAttributes.Public, 
+        AttributeTargetMemberAttributes = MulticastAttributes.Public)]
